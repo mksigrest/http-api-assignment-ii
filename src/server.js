a65ac55a1ede7bdf74a1b6e3208abf3d6e74ba93
mksigrest <mksigrest@gmail.com>
2025-09-28 11:53:51 -0400

    Created base server routes
    added in the client.html page that should load as base, and the style.css page that should load when called

--- src/server.js
+++ src/server.js
@@ -4,7 +4,38 @@ const url = require('url');
 
 const PORT = 3000;
 
+const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true);
+    const path = parsedUrl.pathname;
+    const query = parsedUrl.query;
 
+    if (path === '/' || path === '/client.html') {
+        fs.readFile('client.html', (error, data) => {
+            if (error) {
+                response.writeHead(500, { 'Content-Type': 'text/plain' });
+                response.end('Server error');
+                return;
+            }
+            response.writeHead(200, { 'Content-Type': 'text/html' });
+            response.end(data);
+        });
+        return;
+    }
+
+    else if (path === '/style.css') {
+        fs.readFile('style.css', (error, data) => {
+            if (error) {
+                response.writeHead(404, { 'Content-Type': 'text/plain' });
+                response.end('Not found');
+                return;
+            }
+
+            response.writeHead(200, { 'Content-Type': 'text/css' });
+            response.end(data);
+        });
+        return;
+    }
+}
 
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
