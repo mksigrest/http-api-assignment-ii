7a418aa4663b54362e96f8922a25a18beca1893c
mksigrest <mksigrest@gmail.com>
2025-09-28 11:49:18 -0400

    created base server.js in src file
    created server.js in src file with basic values, and server.listen line

---
+++ src/server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const PORT = 3000;
+
+
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
