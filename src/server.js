f332f92712943e56aa762eac83d3b0067493bbf8
mksigrest <mksigrest@gmail.com>
2025-09-28 20:47:45 -0400

    finalization stuff
    node and eslint stuff

--- src/server.js
+++ src/server.js
@@ -9,58 +9,43 @@ const PORT = 3000;
 const users = {};
 
 const resJSON = (response, statusCode, object) => {
-    response.writeHead(statusCode, { 'Content-Type': 'application/json' });
-    response.end(JSON.stringify(object));
-    return;
+  response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+  response.end(JSON.stringify(object));
 };
 
 const server = http.createServer((request, response) => {
-    const parsedUrl = url.parse(request.url, true);
-    const pathName = parsedUrl.pathname;
+  const parsedUrl = url.parse(request.url, true);
+  const pathName = parsedUrl.pathname;
 
-    if (pathName === '/' || pathName === '/client.html' || pathName === '/style.css') {
-        clientStyle.baseSwitch(pathName, request, response);
-        return;
-    }
+  if (pathName === '/' || pathName === '/client.html' || pathName === '/style.css') {
+    clientStyle.baseSwitch(pathName, request, response);
+  } else if (pathName === '/getUsers' || pathName === '/notReal') {
+    getHead.ghSwitch(pathName, users, request, response);
+  } else if (request.method === 'POST' && pathName === '/addUser') {
+    let body = '';
+    request.on('data', (chunk) => { body += chunk; });
+    request.on('end', () => {
+      let params = {};
+      params = JSON.parse(body);
 
-    else if (pathName === '/getUsers' || pathName === '/notReal') {
-        getHead.ghSwitch(pathName, users, request, response);
-        return;
-    }
-    
-    else if (request.method === 'POST' && pathName === '/addUser') {
-        let body = '';
-        request.on('data', (chunk) => { body += chunk; });
-        request.on('end', () => {
+      const { name, age } = params;
 
-            let params = {};
-            params = JSON.parse(body);
-
-            const { name, age } = params;
-
-            //JSON error
-            if (!name || !age) {
-                resJSON(response, 400, { message: 'Name and age are required', id: 'addUserMissingParams' });
-            }
-
-            else if (!users[name]) {
-                users[name] = { name, age: age };
-                resJSON(response, 201, { message: 'Created Successfully' });
-            }
-
-            else {
-                users[name].age = age;
-                resJSON(response, 204);
-            }
-        });
-        return;
-    }
-
-    else {
-        resJSON(response, 404, { message: "The page you are looking for was not found.", id: "notFound" });
-    }
+      // JSON error
+      if (!name || !age) {
+        resJSON(response, 400, { message: 'Name and age are required', id: 'addUserMissingParams' });
+      } else if (!users[name]) {
+        users[name] = { name, age };
+        resJSON(response, 201, { message: 'Created Successfully' });
+      } else {
+        users[name].age = age;
+        resJSON(response, 204);
+      }
+    });
+  } else {
+    resJSON(response, 404, { message: 'The page you are looking for was not found.', id: 'notFound' });
+  }
 });
 
 server.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}/`);
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
